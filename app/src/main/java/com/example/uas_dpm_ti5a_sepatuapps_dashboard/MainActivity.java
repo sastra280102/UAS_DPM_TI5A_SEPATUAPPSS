a73c33694723a2a4e915060c1d0cf90f394d8dda
sastra280102 <sastrad100@gmail.com>
2022-01-24 23:50:12 +0700

    upload

---
+++ app/src/main/java/com/example/uas_dpm_ti5a_sepatuapps_dashboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uas_dpm_ti5a_sepatuapps_dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
